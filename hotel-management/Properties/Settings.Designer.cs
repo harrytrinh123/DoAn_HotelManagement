062e811c9dc6fa36bdb3a67ba516c68252f0e7dc
jacktrinh123 <hoangtq.iuh@gmail.com>
2021-05-15 16:07:11 +0700

    add clsKetNoi, fix qlKhachSanDataContext

--- hotel-management/Properties/Settings.Designer.cs
+++ hotel-management/Properties/Settings.Designer.cs
@@ -33,5 +33,16 @@ namespace hotel_management.Properties {
                 return ((string)(this["QL_KSConnectionString"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-BRQK3RI\\SQLEXPRESS;Initial Catalog=QLKS;Integrated Security=T" +
+            "rue")]
+        public string QLKSConnectionString {
+            get {
+                return ((string)(this["QLKSConnectionString"]));
+            }
+        }
     }
 }
