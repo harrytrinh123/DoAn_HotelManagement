09b04e197d77574ea9bc9798596364c2406d121e
jacktrinh123 <hoangtq.iuh@gmail.com>
2021-05-16 21:37:29 +0700

    add datacontext

--- hotel-management/Program.cs
+++ hotel-management/Program.cs
@@ -15,7 +15,7 @@ namespace hotel_management
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMainManagement());
+            Application.Run(new frmMain());
         }
     }
 }
