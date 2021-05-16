cd69035f277ade7fa03567862c03265e84597d05
jacktrinh123 <hoangtq.iuh@gmail.com>
2021-05-16 20:41:37 +0700

    fix

--- hotel-management/Program.cs
+++ hotel-management/Program.cs
@@ -15,7 +15,7 @@ namespace hotel_management
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMain());
+            Application.Run(new frmMainManagement());
         }
     }
 }
